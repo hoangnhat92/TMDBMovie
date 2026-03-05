8fe4b4606a02bf015b31d33ae3f034adc2805358
nhatdoan1992 <nhat@coderpush.com>
2026-03-05 09:28:06 +0700

    Initial Commit

---
+++ TMDBMovie/TMDBMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBMovieApp.swift
+//  TMDBMovie
+//
+//  Created by nhat on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TMDBMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
